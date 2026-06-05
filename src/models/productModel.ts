0637261f17663ede8e900448dab08b826a34dce3
Sakshamkhadka7 <sakshamcode12@gmail.com>
2026-06-05 17:42:29 +0545

    added a productModel

---
+++ src/models/productModel.ts
@@ -0,0 +1,56 @@
+import {
+  Table,
+  Column,
+  DataType,
+  Model,
+  PrimaryKey,
+  AllowNull,
+} from "sequelize-typescript";
+
+@Table({
+  tableName: "products",
+  modelName: "Product",
+  timestamps: true,
+})
+class Product extends Model {
+  @Column({
+    primaryKey: true,
+    type: DataType.UUID,
+    defaultValue: DataType.UUIDV4,
+  })
+  productId!: string;
+
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+  })
+  productName!: string;
+
+  @Column({
+    type: DataType.TEXT,
+  })
+  productDescriptions!: string;
+
+  @Column({
+    type: DataType.FLOAT,
+    allowNull: false,
+  })
+  productPrice!: number;
+
+  @Column({
+    type: DataType.INTEGER,
+    allowNull: false,
+  })
+  productTotalStock!: number;
+
+  @Column({
+    type: DataType.FLOAT,
+    allowNull: false,
+  })
+  productDiscount!: number;
+
+  @Column({
+    type: DataType.STRING,
+  })
+  productImage!: string;
+}
