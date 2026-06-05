d7b2c2e5eb2e8abd1405a174bd1c8e75abe9d1de
Sakshamkhadka7 <sakshamcode12@gmail.com>
2026-06-05 18:08:28 +0545

    added CartModel

---
+++ src/models/cartModel.ts
@@ -0,0 +1,24 @@
+import { Table,Column,Model,DataType, AllowNull, Default } from "sequelize-typescript";
+
+@Table({
+    tableName:"carts",
+    modelName:"Cart",
+    timestamps:true
+})
+
+class Cart extends  Model{
+    @Column({
+        primaryKey:true,
+        type:DataType.UUID,
+        defaultValue:DataType.UUID
+    })
+    cartId!:string
+
+    @Column({
+        type:DataType.INTEGER,
+        allowNull:false
+    })
+    quantity!:number
+}
+
+export default Cart
