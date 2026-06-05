1bf328daf22c440dab2be9e01d858c7ccb20ea1c
Sakshamkhadka7 <sakshamcode12@gmail.com>
2026-06-05 17:58:39 +0545

    added orderModel

---
+++ src/globalTypes/index.ts
@@ -0,0 +1,18 @@
+export enum OrderStaus{
+    Prepration="prepration",
+    OntheWay="ontheway",
+    Delivered="delivered",
+    Pending="pending",
+    Cancelled="cancelled"
+}
+
+export enum PaymentMethod{
+khalti="khalti",
+Esewa="eswa",
+COD="cod"
+}
+
+export enum PaymentStatus{
+    Paid="paid",
+    Unpaid="unpaid"
+}
