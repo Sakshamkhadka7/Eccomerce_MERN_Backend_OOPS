5a50ce30b42b85aec0c8c4dedc92a25e9204ff8a
Sakshamkhadka7 <sakshamcode12@gmail.com>
2026-06-10 13:32:37 +0545

    added a userRoute

--- src/app.ts
+++ src/app.ts
@@ -3,5 +3,11 @@ import express from "express";
 const app=express();
 
 import "./database/connection.js";
+import userRouter from "./routes/userRoute.js";
+
+
+app.use(express.json())
+
+app.use("/api/auth",userRouter)
 
 export default app
