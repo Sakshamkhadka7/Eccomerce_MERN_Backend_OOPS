d084959475d3276116de52c7ea9aeb455eacbd06
Sakshamkhadka7 <sakshamcode12@gmail.com>
2026-06-13 23:57:09 +0545

    added relationship of Product and Category

--- src/controllers/ProductController.ts
+++ src/controllers/ProductController.ts
@@ -1,13 +1,12 @@
 import { Request, Response } from "express";
 import Product from "../database/models/productModel.js";
+import Category from "../database/models/categoryModel.js";
 
-
-interface ProductRequest extends Request{
-    file:{
-        filename:string
-    }
+interface ProductRequest extends Request {
+  file: {
+    filename: string;
+  };
 }
-
 
 class ProductController {
   async createController(req: ProductRequest, res: Response): Promise<void> {
@@ -17,9 +16,11 @@ class ProductController {
       productPrice,
       productTotalStock,
       productDiscount,
-      categoryId
+      categoryId,
     } = req.body;
-   const fileName= req.file ? req.file.filename : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTEDgZzszAtkd1ziMCL2WTgmkjb3bIiVEbZLfxpDEtoMZoo3HpahQ3gXpE&s"
+    const fileName = req.file
+      ? req.file.filename
+      : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTEDgZzszAtkd1ziMCL2WTgmkjb3bIiVEbZLfxpDEtoMZoo3HpahQ3gXpE&s";
     if (
       !productName ||
       !productDescriptions ||
@@ -34,14 +35,47 @@ class ProductController {
     }
 
     await Product.create({
-        productName,
+      productName,
       productDescriptions,
       productPrice,
       productTotalStock,
-      productDiscount,
+      productDiscount: productDiscount || 0,
       categoryId,
-      productImage:fileName
-    })
+      productImage: fileName,
+    });
 
+    res.status(200).json({
+      message: "Product created successfully",
+    });
+  }
+
+  async getAllProduct(req: Request, res: Response) {
+    const datas = await Product.findAll({
+      include: {
+        model: Category,
+      },
+    });
+
+    res.status(200).json({
+      message: "Product fetched successfully",
+      data: datas,
+    });
+  }
+
+  async getSingleProduct(req: Request, res: Response) {
+    const { id } = req.params;
+    const datas = await Product.findAll({
+      where: {
+        productId: id,
+      },
+      include: {
+        model: Category,
+      },
+    });
+
+    res.status(200).json({
+      message: "Product fetched successfully",
+      data: datas,
+    });
   }
 }
