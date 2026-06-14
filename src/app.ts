5f897a7be811356042398effcefed55a4709f5d8
Sakshamkhadka7 <sakshamcode12@gmail.com>
2026-06-14 16:07:59 +0545

    added multer

--- src/app.ts
+++ src/app.ts
@@ -5,8 +5,10 @@ const app=express();
 import "./database/connection.js";
 import userRouter from "./routes/userRoute.js";
 import categoryRoute from "./routes/categoryRoute.js";
+import productRoute from "./routes/productRoute.js";
 app.use(express.json())
 app.use("/api/auth",userRouter)
 app.use("/api/category",categoryRoute)
+app.use("/api/product",productRoute)
 
 export default app
--- src/routes/productRoute.ts
+++ src/routes/productRoute.ts
@@ -1,10 +1,15 @@
 import express from "express"
 import ProductController from "../controllers/ProductController.js";
 import userMiddleware, { Role } from "../middleware/userMiddleware.js";
+import upload from "../middleware/upload.js";
+
+ 
+
+
 
 const productRoute=express.Router();
 
-productRoute.route("/addProduct").post(userMiddleware.isUserLogin,userMiddleware.accessTo(Role.Admin),ProductController.createProduct)
+productRoute.route("/addProduct").post(userMiddleware.isUserLogin,userMiddleware.accessTo(Role.Admin),upload.single("image"),ProductController.createProduct)
 productRoute.route("/getProduct").get(ProductController.getAllProduct)
 productRoute.route("/updateProduct/:id").put(userMiddleware.isUserLogin,userMiddleware.accessTo(Role.Admin),ProductController.updateProduct)
 productRoute.route("/deleteProduct/:id").delete(userMiddleware.isUserLogin,userMiddleware.accessTo(Role.Admin),ProductController.deleteProduct)
