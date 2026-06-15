dc12a366a47a00fda19ea71e9acf4e848d790ebc
Sakshamkhadka7 <sakshamcode12@gmail.com>
2026-06-16 01:02:05 +0545

    added global errorHandler

--- src/routes/userRoute.ts
+++ src/routes/userRoute.ts
@@ -1,13 +1,14 @@
 import express, { Router } from "express"
 import UserController from "../controllers/UserController.js"
+import errorHandler from "../services/errorhandler.js";
 
 
 const userRouter:Router=express.Router()
 
-userRouter.route("/register").post(UserController.register)
-userRouter.route("/login").post(UserController.login);
-userRouter.route("/forgot_password").post(UserController.forgetPassword)
-userRouter.route("/verify_otp").post(UserController.verifyOtp)
-userRouter.route("/resetPassword").post(UserController.resetPassword);
+userRouter.route("/register").post(errorHandler(UserController.register))
+userRouter.route("/login").post(errorHandler(UserController.login));
+userRouter.route("/forgot_password").post(errorHandler(UserController.forgetPassword))
+userRouter.route("/verify_otp").post(errorHandler(UserController.verifyOtp))
+userRouter.route("/resetPassword").post(errorHandler(UserController.resetPassword));
 
 export default userRouter
