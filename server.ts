26a1f8e479ec02b5b24f3bf7bbce5ad8e68745b2
Sakshamkhadka7 <sakshamcode12@gmail.com>
2026-06-25 22:26:22 +0545

    added addToOnline user logic in a server.ts

--- server.ts
+++ server.ts
@@ -4,28 +4,58 @@ import app from "./src/app.js";
 import { envConfig } from "./src/config/config.js";
 import adminSeeder from "./adminSeeder.js";
 import categoryController from "./src/controllers/categoryController.js";
-import { Server } from "socket.io";
+import { Server, Socket } from "socket.io";
+import jwt from "jsonwebtoken";
+import User from "./src/database/models/userModel.js";
 
 function startServer() {
   const PORT = envConfig.port || 6000;
 
- const server=app.listen(PORT, () => {
+  const server = app.listen(PORT, () => {
     categoryController.seedCategory();
     console.log(`port is running on ${PORT} port`);
     adminSeeder();
   });
-   
-  const io=new Server(server,{
-    cors:{
-      origin:'http://localhost:5173'
-    }
+
+  const io = new Server(server, {
+    cors: {
+      origin: "http://localhost:5173",
+    },
   });
 
-  io.on("connection",()=>{
-    console.log("Client is connected");
-  })
+  let onlineUsers: { socketId: string; userId: string; role: string }[] = [];
 
+  const addToOnlineUser = (socketId: string, userId: string, role: string) => {
+    onlineUsers = onlineUsers.filter((online) => online.userId !== userId);
+    onlineUsers.push({
+      socketId,
+      userId,
+      role,
+    });
+  };
 
+  io.on("connection", (socket) => {
+    const { token } = socket.handshake.auth;
+    if (token) {
+      jwt.verify(
+        token,
+        envConfig.secretKey as string,
+        async (err: any, result: any) => {
+          if (err) {
+            socket.emit("error", err);
+          } else {
+            const userData = await User.findByPk(result.userId);
+            if (!userData) {
+              socket.emit("Error , No user found with that token");
+              return;
+            }
+
+            addToOnlineUser(socket.id, result.userId, result.role);
+          }
+        },
+      );
+    }
+  });
 }
 
 startServer();
