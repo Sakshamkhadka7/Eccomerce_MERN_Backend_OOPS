608f3e145abed48ba990c941d3dd331bbd9b1f75
Sakshamkhadka7 <sakshamcode12@gmail.com>
2026-06-26 00:06:19 +0545

    completed order between customer and admin real time updated

--- server.ts
+++ server.ts
@@ -7,6 +7,7 @@ import categoryController from "./src/controllers/categoryController.js";
 import { Server, Socket } from "socket.io";
 import jwt from "jsonwebtoken";
 import User from "./src/database/models/userModel.js";
+import Order from "./src/database/models/orderModel.js";
 
 function startServer() {
   const PORT = envConfig.port || 6000;
@@ -35,12 +36,13 @@ function startServer() {
   };
 
   io.on("connection", (socket) => {
-    const { token } = socket.handshake.auth;
+    const token = socket.handshake.headers.token;
     if (token) {
       jwt.verify(
-        token,
+        token as string,
         envConfig.secretKey as string,
         async (err: any, result: any) => {
+          console.log("Result -> ", result);
           if (err) {
             socket.emit("error", err);
           } else {
@@ -50,17 +52,30 @@ function startServer() {
               return;
             }
 
-            addToOnlineUser(socket.id, result.userId, result.role);
+            addToOnlineUser(socket.id, result.userId, userData.role);
+            console.log(onlineUsers);
           }
         },
       );
+    } else {
+      socket.emit("error", "Please provide a token");
     }
 
-    socket.on("updateOrderStatus", (data) => {
+    socket.on("updateOrderStatus", async(data) => {
       const { status, orderId, userId } = data;
 
       const findUser = onlineUsers.find((user) => user.userId == userId);
       if (findUser) {
+      await  Order.update(
+          {
+            orderStaus:status,
+          },
+          {
+            where: {
+              orderId: orderId,
+            },
+          },
+        );
         io.to(findUser.socketId).emit(
           "success",
           "Order status updated successfully",
