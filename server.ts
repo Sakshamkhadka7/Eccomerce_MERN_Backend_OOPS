06b9c5cea86c82600222d5e89c59e413a099c3fa
Sakshamkhadka7 <sakshamcode12@gmail.com>
2026-06-25 21:50:40 +0545

    websocket implemented and connected

--- server.ts
+++ server.ts
@@ -4,15 +4,28 @@ import app from "./src/app.js";
 import { envConfig } from "./src/config/config.js";
 import adminSeeder from "./adminSeeder.js";
 import categoryController from "./src/controllers/categoryController.js";
+import { Server } from "socket.io";
 
 function startServer() {
   const PORT = envConfig.port || 6000;
 
-  app.listen(PORT, () => {
+ const server=app.listen(PORT, () => {
     categoryController.seedCategory();
     console.log(`port is running on ${PORT} port`);
     adminSeeder();
   });
+   
+  const io=new Server(server,{
+    cors:{
+      origin:'http://localhost:5173'
+    }
+  });
+
+  io.on("connection",()=>{
+    console.log("Client is connected");
+  })
+
+
 }
 
 startServer();
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 
+
 const app=express();
 
 import "./database/connection.js";
@@ -7,10 +8,12 @@ import userRouter from "./routes/userRoute.js";
 import categoryRoute from "./routes/categoryRoute.js";
 import productRoute from "./routes/productRoute.js";
 import orderRouter from "./routes/orderRoute.js";
+import cartRouter from "./routes/cartRoute.js";
 app.use(express.json())
 app.use("/api/auth",userRouter)
 app.use("/api/category",categoryRoute)  
 app.use("/api/product",productRoute)
 app.use("/api/order",orderRouter)
+app.use("/api/cart",cartRouter);
 
 export default app
