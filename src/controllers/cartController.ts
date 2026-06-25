487ea3dafbc31b864d7bcfaba1a0cca49efb8a2a
Sakshamkhadka7 <sakshamcode12@gmail.com>
2026-06-25 18:07:07 +0545

    getCart,deleteCart and updateCart is added

--- src/controllers/cartController.ts
+++ src/controllers/cartController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import Cart from "../database/models/cartModel.js";
+import Product from "../database/models/productModel.js";
 
 interface IRequest extends Request {
   user?: {
@@ -27,13 +28,108 @@ class CartController {
 
     if (cartExists) {
       cartExists.quantity += quantity;
-      cartExists.save()
+      cartExists.save();
     } else {
       await Cart.create({
         userId,
         productId,
         quantity,
       });
+
+      res.status(200).json({
+        message: "Product is added to cart",
+      });
+    }
+  }
+
+  static async getMyCarts(req: IRequest, res: Response) {
+    const userId = req.user?.userId;
+
+    const carrItems = await Cart.findAll({
+      where: {
+        userId,
+      },
+      include: [
+        {
+          model: Product,
+          attributes: ["productName", "productPrice", "productImage"],
+        },
+      ],
+    });
+
+    if (carrItems.length === 0) {
+      res.status(400).json({
+        message: "No cart item is found",
+      });
+      return;
+    } else {
+      res.status(200).json({
+        message: "Cart Items fetched",
+        data: carrItems,
+      });
+    }
+  }
+
+  static async deleteCarts(req: IRequest, res: Response) {
+    const userId = req.user?.userId;
+    const { productId } = req.params;
+
+    if (!productId) {
+      res.status(404).json({
+        message: "productId not found",
+      });
+      return;
+    }
+
+    const product = await Product.findByPk(productId);
+    if (!product) {
+      res.status(404).json({
+        message: "product not found",
+      });
+      return;
+    }
+    await Cart.destroy({
+      where: {
+        productId,
+        userId,
+      },
+    });
+
+    res.status(200).json({
+      message: "Cart is deleted successfully",
+    });
+  }
+
+  static async updateItemsQuantity(req: IRequest, res: Response) {
+    const userId = req.user?.userId;
+    const { productId } = req.params;
+    const { quantity } = req.body;
+    if (!quantity) {
+      res.status(400).json({
+        message: "Please provide quantity",
+      });
+      return;
+    }
+    const cartItems = await Cart.findOne({
+      where: {
+        userId,
+        productId,
+      },
+    });
+
+    if (!cartItems) {
+      res.status(404).json({
+        message: "cartItems is not available",
+      });
+      return;
+    } else {
+      cartItems.quantity = quantity;
+      await cartItems.save();
+      res.status(200).json({
+        message:"Cart updated !"
+      })
     }
   }
 }
+
+export default  CartController
